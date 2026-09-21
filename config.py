524366eba7629418f6f7a02d53f17d75c75d62c0
qustareunSPm <qustareun@yandex.ru>
2026-09-21 12:08:02 +0300

    Add configuration loading from .env file

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+if not BOT_TOKEN:
+    raise RuntimeError("Не задан BOT_TOKEN. Создайте файл .env (см. .env.example)")
